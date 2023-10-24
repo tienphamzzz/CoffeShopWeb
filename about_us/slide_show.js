caa8048a979ad9ac51dcb32f6fc4acf2f446ebc9
TienPham <tienpham2kk4@gmail.com>
2023-10-24 15:30:25 +0700

    update link

--- about_us/slide_show.js
+++ about_us/slide_show.js
@@ -1,24 +1,38 @@
+document.addEventListener('DOMContentLoaded', ()=> {
+    const thanhcuon = document.getElementById('thanhcuon');
+    const ND = document.getElementById('nd');
+  
+    document.addEventListener('scroll', () => {
+      const scrollTop = document.documentElement.scrollTop||document.body.scrolltop;
+      const scrollHeight = document.documentElement.scrollHeight;
+      const clientHeight = document.documentElement.clientHeight;
+      const scrollProgress = (scrollTop / (scrollHeight-clientHeight)) * 100;
+      ND.style.width = scrollProgress + '%';
+      console.log(scrollProgress);
+    });
+  });
 
 
-// const anh=document.querySelector('.img');
-// const imgs=document.getElementsByClassName('.vinh')
-// const length=imgs.length
-// let x=0;
-// function chuyen(){
-//     x-=100;
-//     if(x<-200){
-//         x=0; anh.style.transition = 'none';
-//         let width=imgs[0].offsetwidth
-//         anh.style.transform=`translateX(${0}%)`;
-//     }
-//     else {
-//         anh.style.transition = 'transform 0.5s ease'; // Bật lại transition khi chuyển ảnh
-//         let width=imgs[0].offsetwidth
-//         anh.style.transform=`translateX(${x*-1*width}%)`;
-//       }
-//     // anh.style.transform=`translateX(${x}%)`;
-// }
-// setInterval(chuyen,2000);
+
+
+
+
+document.addEventListener('DOMContentLoaded', ()=> {
+    const thanhcuon = document.getElementById('thanhcuon');
+    const ND = document.getElementsByClassName('.lienket');
+  
+    document.addEventListener('scroll', () => {
+      const scrollTop = document.documentElement.scrollTop||document.body.scrolltop;
+      const scrollHeight = document.documentElement.scrollHeight;
+      const clientHeight = document.documentElement.clientHeight;
+      const scrollProgress = (scrollTop / (scrollHeight-clientHeight)) * 100;
+      ND.style.width = scrollProgress + '%';
+    });
+  });
+    
+
+
+
 
 const anh=document.querySelector('.img');
 const imgs=document.querySelectorAll('.vinh')
@@ -40,25 +54,3 @@ setInterval(()=>{
 }
     ,2000)
 
-
-// const anh2=document.querySelector('.anh');
-
-
-// let y=0;
-// function chuyen2(){
-//     y-=100;
-//     if(y<-200){
-//         y=0; anh2.style.transition = 'none';
-        
-//         anh2.style.transform=`translateX(${y}%)`;
-//     }
-//     else {
-//         anh2.style.transition = 'transform 0.5s ease'; // Bật lại transition khi chuyển ảnh
-        
-//         anh2.style.transform=`translateX(${y}%)`;
-//       }
-//     // anh.style.transform=`translateX(${x}%)`;
-// }
-// setInterval(chuyen2,2000);
-  
-  
